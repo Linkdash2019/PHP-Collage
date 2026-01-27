8c07fc12fc71d948857fe744b3edcae3aac16bc4
Linkdash2019 <csmcclellan@outlook.com>
2026-01-26 17:29:11 -0700

    Create variables.php

---
+++ Week 2/variables.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Variables</title>
+  </head>
+  <body>
+    <?php
+      $fName = "Ca...";
+      $lName = "Mc..."; 
+      $address = "987 Candy Cane Ln.";
+      $phoneNum = "123-456-7890";
+      
+      print "<p>Name is $fName $lName <br>
+        who lives at $address<br>
+        with the phone number $phoneNum</p>";
+    ?>
+  </body>
+</html>
