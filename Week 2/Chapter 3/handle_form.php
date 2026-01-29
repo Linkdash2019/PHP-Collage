5cbc0cd5a8d854375d80dac0670c861a515c2c80
Linkdash2019 <csmcclellan@outlook.com>
2026-01-29 16:12:56 -0700

    Feedback2 and handle_form

---
+++ Week 2/Chapter 3/handle_form.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Your Feedback</title>
+  </head>
+  
+  <body>
+    <?php
+    //Put recived data into variables
+      $title = $_POST['title'];
+      $fName = $_POST['fName'];
+      $lName = $_POST['lName'];
+      $response = $_POST['response'];
+      $comments = $_POST['comments'];
+      
+      //Show the recived data
+      print 
+        "<p>
+          Thank you $title $fName $lName for your feedback of: <br>
+          $response <br>
+          $comments
+        </p>"
+    ?>
+  </body>
+</html>
