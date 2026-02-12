71b6951c81e4963ed29143c3e4c836a459480742
Linkdash2019 <csmcclellan@outlook.com>
2026-02-12 12:12:41 -0700

    Create handle_reg.php

---
+++ Week 4/Chapter 6/handle_reg.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Regestration</title>
+  </head>
+  <body>
+    <h1>Regestration Results</h1>
+    <?php
+      $okay = true;
+      
+      if ($okay) {
+        print '<p>Your registration is valid.</p>';
+      }  
+    ?>
+  </body>
+</html>
