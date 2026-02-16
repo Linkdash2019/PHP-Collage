ddabbe63c5280b90e3af22ee97c3c72a4d436fa9
Linkdash2019 <csmcclellan@outlook.com>
2026-02-15 22:40:41 -0700

    Create soups1.php

---
+++ Week 4/Chapter 7/soups1.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Soup</title>
+  </head>
+  <body>
+    <hi>Mmmm...soups</h1>
+    <?php
+      $soups = [
+        'Monday' => 'Clam Chowder',
+        'Tuesday' => 'White Chicken Chili',
+        'Wednesday' => 'Vegetarian'
+      ];
+      
+      print_r($soups); 
+    ?>
+  </body>
+</html>
